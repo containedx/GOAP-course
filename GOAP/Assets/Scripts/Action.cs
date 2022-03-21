e10a9b0f2524eaf636d9975264c4d573bb995575
Kinga <kinga.zawarty@pluckybytes.com>
2022-03-21 19:34:31 +0100

    action

--- GOAP/Assets/Scripts/Action.cs
+++ GOAP/Assets/Scripts/Action.cs
@@ -1,18 +1,73 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.AI;
 using UnityEngine;
 
-public class Action : MonoBehaviour
+public abstract class Action : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public string actionName = "Action";
+    public float cost = 1.0f;
+    public GameObject target; //place when action takes place
+    public GameObject targetTag;
+    public float duration = 0;
+
+    public WorldState[] preconditionsStates;
+    public WorldState[] effectsStates;
+
+    public NavMeshAgent agent;
+
+    public Dictionary<string, int> preconditions;
+    public Dictionary<string, int> effects;
+
+    public WorldStates agentBeliefs;
+
+    public bool running = false;
+
+
+    public Action()
     {
-        
+        preconditions = new Dictionary<string, int>();
+        effects = new Dictionary<string, int>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Awake()
     {
-        
+        agent = gameObject.GetComponent<NavMeshAgent>();
+
+        //take preconditions and effects from WorlStates
+        if(preconditionsStates != null)
+        {
+            foreach(WorldState w in preconditionsStates)
+            {
+                preconditions.Add(w.key, w.value);
+            }
+        }
+        if(effectsStates != null)
+        {
+            foreach(WorldState w in effectsStates)
+            {
+                effects.Add(w.key, w.value);
+            }
+        }
     }
+
+    // Can we do it?
+    public bool IsAchievable()
+    {
+        return true;
+    }
+
+    public bool IsAchievableGiven(Dictionary<string, int> conditions)
+    {
+        foreach(var p in preconditions)
+        {
+            if(!conditions.ContainsKey(p.Key))
+                return false;
+        }
+        return true;
+    }
+
+    public abstract bool PrePerform();
+    public abstract bool PostPerform();
+
 }
--- GOAP/Assets/Scripts/World.cs
+++ GOAP/Assets/Scripts/World.cs
@@ -24,6 +24,6 @@ public class World : MonoBehaviour
 
     public WorldStates GetWorld()
     {
-        return world;
+        return worldStates;
     }
 }
