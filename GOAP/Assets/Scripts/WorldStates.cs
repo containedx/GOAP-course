d6387c0df3d51e6a35d1c5900cf12859e7fdea06
Kinga <kinga.zawarty@pluckybytes.com>
2022-03-21 18:33:38 +0100

    world states representation class

---
+++ GOAP/Assets/Scripts/WorldStates.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class WorldState
+{
+    public string key;
+    public int value;
+}
+
+
+public class WorldStates : MonoBehaviour
+{
+    public Dictionary<string, int> states;
+
+    public WorldStates()
+    {
+        states = new Dictionary<string, int>();
+    }
+
+    public bool HasState(string key)
+    {
+        return states.ContainsKey(key);
+    }
+
+    void AddState(string key, int value)
+    {
+        states.Add(key, value);
+    }
+
+    public void ModifyState(string key, int value)
+    {
+        if(states.ContainsKey(key))
+        {
+            states[key] += value;
+            if(states[key] <= 0)
+            {
+                //remove state if it's amount below 0 => its not existing
+                RemoveState(key);
+            }
+            else
+            {
+                //if not exist, then just add
+                states.Add(key, value);
+            }
+
+        }
+    }
+
+    public void RemoveState(string key)
+    {
+        if(states.ContainsKey(key))
+            states.Remove(key);
+    }
+
+    public void SetState(string key, int value)
+    {
+        if(states.ContainsKey(key))
+            states[key] = value;
+        else
+            states.Add(key, value);
+    }
+
+    public Dictionary<string, int> GetAllStates()
+    {
+        return states;
+    }
+}
