ac53756e2d9548541500baf4c01151f24b5efa2d
Kinga <kinga.zawarty@pluckybytes.com>
2022-03-21 20:15:14 +0100

    agent

--- GOAP/Assets/Scripts/Agent.cs
+++ GOAP/Assets/Scripts/Agent.cs
@@ -1,18 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.AI;
 using UnityEngine;
+using System.Linq;
+
+
+public class SubGoal
+{
+    public Dictionary<string, int> goals;
+    public bool remove;
+
+    public SubGoal(string s, int i, bool r)
+    {
+        goals = new Dictionary<string, int>();
+        goals.Add(s,i);
+        remove = r;
+    }
+}
 
 public class Agent : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public List<Action> actions = new List<Action>();
+    public Dictionary<SubGoal, int> goals = new Dictionary<SubGoal, int>();
+
+    Planner planner;
+
+    Queue<Action> actionQueue;
+    public Action currentAction;
+    SubGoal currentGoal;
+
+
     void Start()
+    {
+        //get all actions
+        Action[] act = GetComponents<Action>();
+        foreach(Action a in act)
+            actions.Add(a);
+
+    }
+
+    void LateUpdate()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
